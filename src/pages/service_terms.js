e5c0e176307c600a009cb0e736cd4eceede583bb
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-07-17 20:56:17 -0500

    Primer entrega

---
+++ src/pages/service_terms.js
@@ -0,0 +1,110 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const service_terms = () => {
+  return (
+    <Layout>
+      <article>
+        <h1>Condiciones del Servicio</h1>
+        <p>
+          El uso del sitio Justicia Digital en Internet
+          www.justiciadigital.gob.mx o de cualquiera de sus componentes, implica
+          la aceptación expresa de los siguientes:
+        </p>
+        <h2>Términos de uso del sitio web</h2>
+        <ul>
+          <li>
+            El Poder Judicial del Estado de Coahuila de Zaragoza siempre ha
+            otorgado la mayor importancia a difundir información de calidad,
+            apegada a las mejores metodologías, por ello ha creado este sitio en
+            Internet para facilitar a los usuarios el acceso a la información
+            que genera y recopila.
+          </li>
+          <li>
+            Este sitio contiene medidas de seguridad para proteger la
+            información de cualquier alteración realizada por terceros, sin
+            embargo, el Poder Judicial del Estado de Coahuila de Zaragoza no
+            asume ninguna responsabilidad por la alteración o manipulación de
+            los datos una vez publicados en él.
+          </li>
+          <li>
+            Este sitio en Internet ofrece enlaces a otros organismos nacionales
+            e internacionales. No obstante, el Poder Judicial del Estado de
+            Coahuila de Zaragoza no asume ningún tipo de responsabilidad por el
+            contenido, ni por el uso de esos sitios.
+          </li>
+          <li>
+            Las leyes, reglamentos y demás disposiciones que aparecen en el
+            sitio no crean derechos ni establecen obligaciones distintas de las
+            contenidas en las disposiciones publicadas en el Diario Oficial de
+            la Federación.
+          </li>
+        </ul>
+        <h2>El Usuario</h2>
+        <ul>
+          <li>
+            Puede utilizar la información del Poder Judicial del Estado de
+            Coahuila de Zaragoza con apego a lo dispuesto en los Términos de Uso
+            de la Información.
+          </li>
+          <li>
+            Se obliga a hacer buen uso del sitio, respetando la normatividad
+            vigente en la materia, y a no causar daños en el mismo.
+          </li>
+          <li>
+            Debe contar con un equipo que cumpla con las características mínimas
+            necesarias para navegar en el sitio.
+          </li>
+          <li>
+            Exime al Poder Judicial del Estado de Coahuila de Zaragoza de toda
+            responsabilidad por los daños que el uso del sitio le pudieran
+            ocasionar en forma incidental o consecuente con su equipo,
+            información, patrimonio o persona.
+          </li>
+          <li>
+            Acepta y se obliga a utilizar el sitio para fines lícitos y con
+            apego a las disposiciones legales aplicables.
+          </li>
+        </ul>
+        <h2>El Poder Judicial del Estado de Coahuila de Zaragoza</h2>
+        <ul>
+          <li>
+            No garantiza el funcionamiento de la red de comunicaciones y por lo
+            tanto no asume responsabilidad alguna sobre la disponibilidad de
+            este sitio.
+          </li>
+          <li>No garantiza el suministro continuo de la información.</li>
+          <li>
+            No se responsabiliza de ningún daño o perjuicio sufrido por el
+            usuario que se derive del acceso a los servidores institucionales o
+            del uso de información o aplicaciones en ellos contenidos. Aunque el
+            material contenido en este sitio es proporcionado como archivos de
+            estándares reconocidos, como HTML, CSV y PDF, el Poder Judicial del
+            Estado de Coahuila de Zaragoza le recomienda tomar precauciones para
+            proteger su equipo.
+          </li>
+        </ul>
+        <p>
+          La vigencia de estos Términos de Uso es por tiempo indefinido siempre
+          que no se contravengan las disposiciones anteriores.
+        </p>
+        <p>
+          El uso no autorizado en contravención de estos Términos de Uso, podrá
+          ser sancionado de acuerdo con la legislación vigente aplicable.
+        </p>
+        <p>
+          Se reserva el derecho, en cualquier momento y sin previa notificación
+          al Usuario, a modificar ampliar o suspender temporalmente la
+          presentación, configuración, especificaciones técnicas y servicios del
+          sitio web, de forma unilateral.
+        </p>
+        <p>
+          Se reserva el derecho a modificar en cualquier momento los presentes
+          Términos de Uso.
+        </p>
+      </article>
+    </Layout>
+  );
+};
+
+export default service_terms;
